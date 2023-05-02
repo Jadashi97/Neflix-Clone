4abb356130ee60223e360a71edfd78ae3e324562
jadashi97 <njadauniversal23@yahoo.com>
2023-05-02 15:02:55 -0500

    (feat): added rows for each movie category. (fix): solve why movies not showing on the page

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,19 @@ function App() {
   return (
     <div>
       <h1>Hello! Time to build Netflix Clone!</h1>
-      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}/>
+      <Row 
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}/>
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries}/>
     </div>
   )
 }
--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,7 +1,9 @@
 import React, {useEffect, useState} from 'react';
 import axios from "../../axios";
 
-const Row = ({title, fetchUrl}) => {
+const base_url = "https://image.tmdb.org/t/p/original"
+
+const Row = ({title, fetchUrl, isLargeRow}) => {
 
     const [movies, setMovies] = useState([]);
 
@@ -9,15 +11,27 @@ const Row = ({title, fetchUrl}) => {
     useEffect(() =>{
         async function fetchData(){
             const request = await axios.get(fetchUrl);
-            console.log(request);
+            setMovies(request.data.results);
             return request;
         }
         fetchData();
-    }, [])
+    }, [fetchUrl]);
+
+    console.log(movies);
 
     return (
-        <div>
-        <h2>{title}</h2>
+        <div className='row'>
+            <h2>{title}</h2>
+            <div className='row_posters'>
+                {/* several rows_posters */}
+                {movies.map((movie)=> {
+                    <img 
+                        key={movie.id}
+                        src={`${base_url}${movie.poster_path}`} 
+                        alt={movie.name}
+                    />
+                })}
+            </div>
         </div>
     )
 }
