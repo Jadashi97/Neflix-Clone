99a8d8c6da39cd45000df2d07724849e501c5dab
jadashi97 <njadauniversal23@yahoo.com>
2023-05-03 15:02:17 -0500

    (feat): added the movies to display in rows. (fix): solve width and height of the page

--- requests.js
+++ requests.js
@@ -11,6 +11,7 @@ const requests = {
   fetchHorrorMovies: `/discover/movie?api_key=${apiKey}&with_genres=27`,
   fetchRomanceMovies: `/discover/movie?api_key=${apiKey}&with_genres=10749`,
   fetchDocumentaries: `/discover/movie?api_key=${apiKey}&with_genres=99`,
-}
+
+};
 
 export default requests;
