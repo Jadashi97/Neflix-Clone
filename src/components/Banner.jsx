4feb0dd7aae39f7e058329920df7ec314b570045
jadashi97 <njadauniversal23@yahoo.com>
2023-05-06 09:36:53 -0500

    (feat): added fade to Banner & function to truncate long description.(fix): add more components

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -20,6 +20,11 @@ const Banner = () => {
     }, []);
 
     console.log(movie);
+    
+    // this cuts down on movies with long descriptions on the page
+    function truncate(str, n){
+        return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+    }
 
     return (
         <header 
@@ -41,8 +46,11 @@ const Banner = () => {
                     <button className='banner_button'>Play</button>
                     <button className='banner_button'>My List</button>
                 </div>
-                <h1 className='banner_description'>{movie?.overview}</h1>
+                <h1 className='banner_description'>
+                    {truncate(movie?.overview, 150)}
+                </h1>
             </div>
+            <div className='banner--fadeBottom'/>
         </header>
     )
 }
