22fdd19caa7aa5221a958ada45c3f8c551212ba5
jadashi97 <njadauniversal23@yahoo.com>
2023-05-09 11:26:39 -0500

    (feat): app is deployed. (fix): solve why I cant view deployed app

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,68 +1,70 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 import axios from "../../axios";
 import ".././styles.css/Row.css";
 import YouTube from "react-youtube";
-import movieTrailer from 'movie-trailer';
+import movieTrailer from "movie-trailer";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
-const Row = ({title, fetchUrl, isLargeRow}) => {
-    const [movies, setMovies] = useState([]);
-    const [trailerUrl, setTrailerUrl] = useState("");
+const Row = ({ title, fetchUrl, isLargeRow }) => {
+  const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
-    // A snippet of code which runs a specific condition
-    useEffect(() =>{
-        async function fetchData(){
-            const request = await axios.get(fetchUrl);
-            setMovies(request.data.results);
-            return request;
-        }
-        fetchData();
-    }, [fetchUrl]);
+  // A snippet of code which runs a specific condition
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
 
-    // console.table(movies);
+  // console.table(movies);
 
-    // sets the height and width for the trailer to be played
-    const opts = {
-        height: "390",
-        width: "100%",
-        playerVars: {
-            autoplay: 1,
-        },
-    };
+  // sets the height and width for the trailer to be played
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
 
-    // handling when movie is clicked and shows trailer then stops when clicked again
-    const handleClick = ((movie) => {
-        if(trailerUrl){
-            setTrailerUrl("");
-        }else{
-            movieTrailer(movie?.name || "")
-            .then((url) => {
-                const urlParams = new URLSearchParams(new URL(url).search)
-                setTrailerUrl(urlParams.get("v"));
-            })
-            .catch((error) => console.log(error));
-        }
-    })
+  // handling when movie is clicked and shows trailer then stops when clicked again
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl(" ");
+    } else {
+      movieTrailer(movie?.name || " ")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+    }
+  };
 
-    return (
-        <div className='row'>
-            <h2>{title}</h2>
-            <div className='row_posters'>
-                {/* several rows_posters */}
-                {movies.map((movie)=> (
-                    <img 
-                        key={movie.id}
-                        onClick={() => handleClick(movie)}
-                        className={`row_poster ${isLargeRow && "row_posterLarge"}`}
-                        src={`${base_url}${ isLargeRow ? movie.poster_path : movie.backdrop_path}`} 
-                        alt={movie.name}
-                    />
-                ))}
-            </div>
-            {trailerUrl && <YouTube videoId={trailerUrl} opts={opts}/>}
-        </div>
-    )
-}
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="row_posters">
+        {/* several rows_posters */}
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            onClick={() => handleClick(movie)}
+            className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+    </div>
+  );
+};
 
 export default Row;
